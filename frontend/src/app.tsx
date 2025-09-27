d5cfe5dfb044ffb04d37ee96dec00b6edb7205f4
Cripacx <cripacx@gmail.com>
2025-09-27 12:55:22 +0200

    working

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState, useMemo} from 'react';
+import {useState, useMemo, useEffect} from 'react';
 import {createRoot} from 'react-dom/client';
 import Map, {
   Marker,
@@ -12,47 +12,70 @@ import Map, {
 
 import ControlPanel from './control-panel';
 import Pin from './pin';
+import {config, api} from './config';
 
-import CITIES from '../../../.data/cities.json';
-
-const TOKEN = ''; // Set your mapbox token here
+import CITIES from '../cities.json';
 
 export default function App() {
   const [popupInfo, setPopupInfo] = useState(null);
+  const [boxes, setBoxes] = useState([]);
 
-  const pins = useMemo(
-    () =>
-      CITIES.map((city, index) => (
+  const handleFabClick = () => {
+    // Hier können Sie die gewünschte Aktion hinzufügen
+    console.log('FAB clicked!');
+    // Beispiel: Zur aktuellen Position navigieren oder eine Aktion ausführen
+  };
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await api.getBoxes(
+          config.defaultLocation.latitude,
+          config.defaultLocation.longitude
+        );
+        setBoxes(await data.json());
+      } catch (error) {
+        console.error('Error loading boxes:', error);
+        setBoxes([]);
+      }
+    }
+    load();
+  }, []);
+
+  useEffect(() => {
+    console.log('Boxes updated:', boxes);
+  }, [boxes]);
+
+  const pins = 
+      boxes.map((box, index) => (
         <Marker
           key={`marker-${index}`}
-          longitude={city.longitude}
-          latitude={city.latitude}
+          longitude={box.point.longitude}
+          latitude={box.point.latitude}
           anchor="bottom"
           onClick={e => {
             // If we let the click event propagates to the map, it will immediately close the popup
             // with `closeOnClick: true`
             e.originalEvent.stopPropagation();
-            setPopupInfo(city);
+            setPopupInfo(box);
           }}
         >
           <Pin />
         </Marker>
-      )),
-    []
-  );
+      ));
 
   return (
     <>
       <Map
         initialViewState={{
-          latitude: 40,
-          longitude: -100,
-          zoom: 3.5,
+          latitude: config.defaultLocation.latitude,
+          longitude: config.defaultLocation.longitude,
+          zoom: config.defaultLocation.zoom,
           bearing: 0,
           pitch: 0
         }}
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        mapboxAccessToken={TOKEN}
+        mapStyle={config.mapbox.defaultStyle}
+        mapboxAccessToken={config.mapbox.token}
       >
         <GeolocateControl position="top-left" />
         <FullscreenControl position="top-left" />
@@ -64,25 +87,24 @@ export default function App() {
         {popupInfo && (
           <Popup
             anchor="top"
-            longitude={Number(popupInfo.longitude)}
-            latitude={Number(popupInfo.latitude)}
+            longitude={Number(popupInfo.point.longitude)}
+            latitude={Number(popupInfo.point.latitude)}
             onClose={() => setPopupInfo(null)}
           >
             <div>
-              {popupInfo.city}, {popupInfo.state} |{' '}
-              <a
-                target="_new"
-                href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
-              >
-                Wikipedia
-              </a>
+              {JSON.stringify(popupInfo)} |{' '}
             </div>
-            <img width="100%" src={popupInfo.image} />
           </Popup>
         )}
       </Map>
 
       <ControlPanel />
+      
+      <button className="fab" onClick={handleFabClick} title="Neue Aktion">
+        <svg viewBox="0 0 24 24">
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+        </svg>
+      </button>
     </>
   );
 }
