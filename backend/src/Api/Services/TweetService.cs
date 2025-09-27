b0b92a65de8c7065c63cb042eb181cc51b7bdd63
PlaySkyHD <nic14markfort@gmail.com>
2025-09-27 13:46:18 +0200

    feat: Add AI

--- backend/src/Api/Services/TweetService.cs
+++ backend/src/Api/Services/TweetService.cs
@@ -8,25 +8,37 @@ using Azure.Storage.Sas;
 
 namespace Api.Services;
 
-public sealed class TweetService(IBoxRepository boxRepository, ITweetRepository tweetRepository, IBlobClient blobClient) : ITweetService
+public sealed class TweetService(ILogger<TweetService> logger,IAiService aiService, IBoxRepository boxRepository, ITweetRepository tweetRepository, IBlobClient blobClient) : ITweetService
 {
 
     public async Task UploadTweet(Guid boxId, CreateTweetRequest request)
     {
         await boxRepository.GetBoxElseThrow(boxId);
         using MemoryStream stream = new(Convert.FromBase64String(request.ImageBase64));
+
+        AIResponse? response = null;
+        try
+        {
+            response = await aiService.AnalyzeImageAsync(new AIRequest() { Image = stream });
+            stream.Position = 0;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, e.Message);
+        }
+        
         string fileName = Guid.CreateVersion7().ToString();
         await blobClient.UploadData(fileName, stream);
         TweetEntity entity = new()
         {
-            BirdType = request.BirdType,
+            BirdType = request.BirdType ?? response?.BirdType,
             BoxId = boxId,
             UploadedAt = DateTime.UtcNow,
             BlobName = fileName,
             Description = request.Description,
-            EggCount = request.EggCount,
-            HatchedCount = request.HatchedCount,
-            IsOccupied = request.IsOccupied,
+            EggCount = request.EggCount ?? response?.EggCount,
+            HatchedCount = request.HatchedCount ?? response?.HatchedCount,
+            IsOccupied = request.IsOccupied ?? response?.IsOccupied,
             DeadCount = request.DeadCount
         };
         await tweetRepository.AddTweet(entity);
